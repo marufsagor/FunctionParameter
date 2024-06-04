56602c627a04ef2509dfa27c3f9524ff139b2567
Maruf Sagor <marufsagor10@gmail.com>
2024-06-04 18:18:31 +0600

    Add files via upload

---
+++ src/main/java/org/example/AddClass.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class AddClass {
+
+    public void add(int a, int b){
+        int c=a+b;
+        System.out.println("Output is "+c);
+
+
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("");
+        AddClass kk = new AddClass();
+        kk.add(10,20);
+
+    }
+}
